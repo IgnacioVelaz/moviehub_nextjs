86775a1d62359def84b3ccf6d6849acd309c96d0
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2023-12-14 09:40:41 +0100

    styles: change color to active navLink

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
-
-const inter = Inter({ subsets: ["latin"] });
+import fontPrimary from "./fonts";
 
 export const metadata: Metadata = {
   title: "MovieHub",
@@ -18,7 +16,7 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${fontPrimary.className} antialiased`}>{children}</body>
     </html>
   );
 }
