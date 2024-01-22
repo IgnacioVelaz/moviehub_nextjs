fdcd854ab3a62fb8cea216e4d9ccb06a54348f2e
Ignacio Velazquez <ji.velazquezpp@gmail.com>
2024-01-22 22:01:28 +0100

    feat: disable add buttons on movie page when movie is available in the context

--- src/app/(main)/movie/[movieId]/components/movie-page-controlers.tsx
+++ src/app/(main)/movie/[movieId]/components/movie-page-controlers.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { TmdbMovie } from "@/app/(main)/discover/models";
+import { MoviesContext } from "@/context/movies-context";
 import { MovieDetailsInterface } from "@/interfaces/MovieDetailsInterface";
+import { MovieInterfaceDB } from "@/interfaces/MovieInterfaceDB";
 import movieDetailsToTmdb from "@/utils/movie-details-to-tmdb-movie";
 import movieFunctions from "@/utils/movie-functions";
-import { useRouter } from "next/navigation";
-import { FC } from "react";
+import tmdbToDbMovie from "@/utils/tmdb-to-db-movie";
+import { FC, useContext } from "react";
 import { FaEye } from "react-icons/fa";
 import { IoAddCircleSharp } from "react-icons/io5";
 
@@ -14,18 +17,30 @@ type MoviePageControlersProps = {
 
 const MoviePageControlers: FC<MoviePageControlersProps> = ({ movie }) => {
   const { addMovieToList } = movieFunctions;
-  const router = useRouter();
-
   const tmdbMovie = movieDetailsToTmdb(movie);
 
-  console.log("MOVIE", movie);
+  const { userMovies, setUserMovies } = useContext(MoviesContext);
+
+  const alreadySavedMovie = userMovies.find(
+    (contextMovie) => contextMovie.tmdb_id === movie.id,
+  );
+
+  const isDisabled = !!alreadySavedMovie;
+
+  const handleAddMovie = (movieToAdd: TmdbMovie, list: string) => {
+    const dbMovie = tmdbToDbMovie(movieToAdd, list);
+    addMovieToList(tmdbMovie, list);
+    setUserMovies((prevMovies: MovieInterfaceDB[]) => [...prevMovies, dbMovie]);
+  };
 
   return (
     <div className="flex justify-center items-center gap-4">
       <button
+        className="disabled:opacity-50 disabled:pointer-events-none"
+        disabled={isDisabled}
         type="button"
         onClick={() => {
-          addMovieToList(tmdbMovie, "watchlist", router);
+          handleAddMovie(tmdbMovie, "watchlist");
         }}
         aria-label="Add to Watchlist"
       >
@@ -33,9 +48,11 @@ const MoviePageControlers: FC<MoviePageControlersProps> = ({ movie }) => {
       </button>
 
       <button
+        className="disabled:opacity-50 disabled:pointer-events-none"
+        disabled={isDisabled}
         type="button"
         onClick={() => {
-          addMovieToList(tmdbMovie, "watched", router);
+          handleAddMovie(tmdbMovie, "watched");
         }}
         aria-label="Add to Watched"
       >
